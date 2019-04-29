ff685fd03173adbecb300196450ea002aa120556
FelipeCasanova <felipe.casanova.ua@gmail.com>
2019-04-29 18:04:52 +0100

    Adding few resources to the Identity.API service.

--- src/Services/Pages/Pages.API/Controllers/PagesController.cs
+++ src/Services/Pages/Pages.API/Controllers/PagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Pages.API.Infrastructure.Services;
 using Pages.API.Model;
@@ -10,6 +11,7 @@ using Pages.API.Model;
 namespace Pages.API.Controllers
 {
     [Route("api/v1/[controller]")]
+    [Authorize]
     [ApiController]
     public class PagesController : ControllerBase
     {
